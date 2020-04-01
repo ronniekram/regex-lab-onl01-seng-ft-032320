b7372b4da4e50b0ca46c1d0ec429fac3a2508f6c
Arynn Boniface <arynn.boniface@gmail.com>
2020-04-01 21:16:20 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -15,5 +15,5 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  !!phone.scan(/\W*([0-9]){3}\W*([0-9]){3})\W*([0-9]{4})?/)
+  !!phone.scan(/\W*([0-9]{3})\W*([0-9]{3})\W*([0-9]{4})?/)
 end
